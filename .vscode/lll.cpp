ea4af36f365f72a88cc5bad04e8861f93fb77e50
Abhinav Saxena <abhinav150601@gmail.com>
2022-07-13 14:34:19 +0530

    sd

---
+++ .vscode/lll.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "Jack";
+    int characterAge;
+    characterAge = 24;
+    cout << "Hello world!" << endl;
+    cout << "My name is" << characterName << endl;
+    cout << "I am " << characterAge << " years old." << endl;
+    cout << characterName << " is typing this code for the first time." << endl;
+
+
+    return 0;
+}
